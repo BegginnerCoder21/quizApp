39b5f8734ea563815520ddef49e12e12a0d0f378
Teguera <aboubacarteguera21@gmail.com>
2023-08-12 15:02:49 +0200

    debut du projet

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+  import Home from '@/components/views/Home.vue'
+</script>
+
+<template>
+    <main>
+      <Home />
+    </main>
+</template>
+
+<style>
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #252222;
+}
+</style>
---
+++ src/components/views/Home.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="app">
+        <h1>The Quiz</h1>
+        <div class="quizContent">
+            <div class="question">
+                <h3>What is vue?</h3>
+                <span>score 1/3</span>
+            </div>
+            <div class="quizanswers">
+                fdjslsjdkfsqjkdjfsl
+            </div>
+            <div class="quizanswers">
+                fdjslsjdkfsqjkdjfsl
+            </div>
+            <div class="quizanswers">
+                fdjslsjdkfsqjkdjfsl
+            </div>
+            <button v-if="isSeleted" :disabled="isSeleted">SELECT AN OPTION</button>
+            <button v-if="!isSeleted">NEXT QUESTION</button>
+            <button v-if="!isSeleted">FINISH</button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import {ref} from 'vue'
+const isSeleted = ref<boolean>(true)
+</script>
+
+<style scoped>
+h1{
+    color: #fff;
+}
+
+button{
+    padding: 7px 20px;
+    display: flex;
+    justify-content: start;
+    background-color: green;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+    margin-top: 7px;
+}
+
+.app{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+    flex-direction: column;
+}
+
+.question{
+    display: flex;
+    justify-content: space-between;
+    width: 500px;
+    flex-direction: row;
+    background-color: #313131;
+}
+
+h3{
+    background-color: #313131;
+    color: #71717a;
+}
+
+span{
+    background-color: #313131;
+    color:  #fff;
+}
+.quizanswers{
+    height: 50px;
+    width: 500px;
+    margin-top: 10px;
+    background-color: #252222;
+    align-items: center;
+    cursor: pointer ;
+    padding: 10px;
+    border-radius: 4px;
+    display: flex;
+    color: #fff;
+    font-size: 12px;
+}
+.quizContent{
+    border-radius: 2px;
+    padding: 10px;
+    height: 260px;
+    width: auto;
+    background-color: #313131;
+    margin-top: 20px;
+}
+</style>
